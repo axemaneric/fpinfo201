a3375654d319e31b5359780bf566cc3d00752927
Chris Forbes <cforbes1@uw.edu>
2018-05-23 15:32:20 -0700

    Filtered data. Added comments

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -2,13 +2,26 @@ library(dplyr)
 library(jsonlite)
 library(httr)
 
+# Convert JSON into data frame (Category ID to Category conversion)
 data <- fromJSON("../data/US_category_id.json")
 data <- flatten(data[["items"]]) %>% 
   select(id, snippet.title) %>% 
-  rename(category_id = id, Category = snippet.title)
-data$category_id <- as.integer(data$category_id)
-US_videos <- read.csv("../data/USvideos.csv", stringsAsFactors = FALSE)
-new_data <- left_join(US_videos, data, by = "category_id")
-l <- as.character(data$Category)
-names(l) <- data$category_id
+  rename(category_id = id, category = snippet.title)
 
+# Convert category id from char to int
+data$category_id <- as.integer(data$category_id)
+
+# Read in data of Top 200 US Videos
+US_videos <- read.csv("../data/USvideos.csv", stringsAsFactors = FALSE)
+
+# Join `US_videos` with `data` by category id. Adds categories to the videos
+new_data <- left_join(US_videos, data, by = "category_id")
+
+# Filter out videos we can't use (videos without comments, ratings, or deleted videos)
+# Select only columns that we need
+new_data <- new_data %>% 
+  filter(comments_disabled == 'False') %>%
+  filter(ratings_disabled == 'False') %>%
+  filter(video_error_or_removed == 'False') %>%
+  select(video_id, trending_date, title, channel_title, category, tags, 
+         views, likes, dislikes, comment_count, thumbnail_link)
