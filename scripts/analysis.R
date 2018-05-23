4593364089b5f567d13ee1f574bfbb81cf9c0c9c
BrianHo <Curry30@D-10-19-153-228.dhcp4.washington.edu>
2018-05-23 14:30:52 -0700

    replaced id with category

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -6,7 +6,7 @@ data <- fromJSON("../data/US_category_id.json")
 data <- flatten(data[["items"]]) %>% 
   select(id, snippet.title) %>% 
   rename(category_id = id, Category = snippet.title)
-
+data$category_id <- as.integer(data$category_id)
 US_videos <- read.csv("../data/USvideos.csv", stringsAsFactors = FALSE)
 new_data <- left_join(US_videos, data, by = "category_id")
 l <- as.character(data$Category)
