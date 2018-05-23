f6a749a720b5cbfe4f1d0a73ba2af1ab16d85f8a
Eric Fan <ericfan1999@hotmail.com>
2018-05-23 14:15:45 -0700

    pre

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -1,4 +1,14 @@
 library(dplyr)
 library(jsonlite)
+library(httr)
 
-  
+data <- fromJSON("../data/US_category_id.json")
+data <- flatten(data[["items"]]) %>% 
+  select(id, snippet.title) %>% 
+  rename(category_id = id, Category = snippet.title)
+
+US_videos <- read.csv("../data/USvideos.csv", stringsAsFactors = FALSE)
+new_data <- left_join(US_videos, data, by = "category_id")
+l <- as.character(data$Category)
+names(l) <- data$category_id
+
