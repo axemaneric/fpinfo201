6d6e9963bdd0b7b9d28842af5fe76444e7a95c0f
Xueqi Xia <emmaxia@Xueqis-MacBook-Pro.local>
2018-05-29 20:16:41 -0700

    hopefully final line plot

--- scripts/build_line.R
+++ scripts/build_line.R
@@ -4,10 +4,6 @@ library(lubridate)
 source("analysis.R")
 
 build_line <- function(my_data, xvar, yvar){
-  #testing
-  my_data <- new_data
-  xvar <- list("2017-11-30", "2018-01-01")
-  yvar <- list("Entertainment", "People & Blogs", "Sports")
   
   # filter based on date slider
   date_one <- as.Date(xvar[[1]])
@@ -24,7 +20,7 @@ build_line <- function(my_data, xvar, yvar){
   # draw out the first line based on the data
   my_line <- plot_ly(data, x = ~trending_date, y = ~video,
                      type = "scatter", name = line_one, mode = "line") 
-
+  
   # draw out the rest of the lines based on other categories
   for (i in 1 : (length(yvar)-1)){
     col_name <- paste0("video", i)
@@ -34,15 +30,17 @@ build_line <- function(my_data, xvar, yvar){
       summarize(video = length(video_id))
     data <- data %>% 
       mutate(!!col_name := new_data$video)
+    yaxis = data[[col_name]]
     my_line <- my_line %>% 
-      add_trace(y = col_name, name = line_name, mode = "line")
+      add_trace(y = yaxis, name = line_name, type = "scatter", mode = "line")
   }
   
+  # formatting the line plot
   my_line <- my_line %>% 
     layout(title = "Number of Trending Videos in Catogories Change in Time",
            xaxis = list(title = "Date"),
            yaxis = list (title = "Number of Videos"))
-  
+  my_line
 
 }
 
