ff80dfbb25d2c95a333073fd966e4fca0af15f06
Xueqi Xia <emmaxia@Xueqis-MacBook-Pro.local>
2018-05-30 12:38:56 -0700

    testing

--- scripts/build_line.R
+++ scripts/build_line.R
@@ -3,19 +3,23 @@ library(plotly)
 library(lubridate)
 
 build_line <- function(my_data, xvar, yvar){
+  #testing
+  my_data <- new_data
+  xvar <- list("2017-11-30", "2018-01-01")
+  yvar <- list("Entertainment", "People & Blogs", "Sports")
   
   # filter based on date slider
   date_one <- as.Date(xvar[[1]])
   date_two <- as.Date(xvar[[2]])
   my_data <- my_data %>% 
     filter(trending_date > date_one & trending_date < date_two)
-
+  
   # filter out the data related to the last category in the list
   data <- filter(my_data, category == yvar[[length(yvar)]]) %>%
     group_by(trending_date) %>%
     summarize(video = length(video_id))
   line_one <- yvar[[length(yvar)]]
-
+  
   # draw out the first line based on the data
   my_line <- plot_ly(data, x = ~trending_date, y = ~video,
                      type = "scatter", name = line_one, mode = "line") 
@@ -40,6 +44,6 @@ build_line <- function(my_data, xvar, yvar){
            xaxis = list(title = "Date"),
            yaxis = list (title = "Number of Videos"))
   my_line
-
+  
 }
 
